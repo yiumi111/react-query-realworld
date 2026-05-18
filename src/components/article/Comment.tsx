27b71a072f227e2c2a3201d83f77f58b6ec8265d
yiumi111 <2591292671@qq.com>
2026-05-18 05:48:38 +0000

    fdhfgchfc

--- src/components/article/Comment.tsx
+++ src/components/article/Comment.tsx
@@ -20,12 +20,17 @@ const Comment = ({ comments, slug }: ICommentProps) => {
   const onPostComment = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const { body } = newComment;
+
+    if (!body.trim()) {
+      return;
+    }
+
     createCommentMutation.mutate(
       { body, slug },
       {
         onSuccess: (_) => {
-          setNewComment({ body: '', slug });
-          queryClient.invalidateQueries({ queryKey: [QUERY_COMMENTS_KEY] });
+          setNewComment({ body: '' });
+          queryClient.invalidateQueries({ queryKey: [QUERY_COMMENTS_KEY, slug] });
         },
       },
     );
@@ -57,7 +62,7 @@ const Comment = ({ comments, slug }: ICommentProps) => {
         </div>
         <div className="card-footer">
           <img src={data.image} className="comment-author-img" alt="comment-author" />
-          <button type="submit" className="btn btn-sm btn-primary">
+          <button type="submit" className="btn btn-sm btn-primary" disabled={createCommentMutation.isLoading}>
             Post Comment
           </button>
         </div>
